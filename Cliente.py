1bc141741a740b60e4a7d4a19c3ef614b25fa684
Karen-Palacios2007 <palaciosrojaskarendaniela@gmail.com>
2025-03-14 20:53:19 -0500

    versión 2.0
    Creación de clase cliente, principal y saludo

---
+++ Cliente.py
@@ -0,0 +1,31 @@
+#crear clase
+class Cliente:
+    def __init__(self):
+        self.nomb_cliente=""
+        self.apell_cliente=""
+
+    #Se crean metodos Get y Set para atributos
+
+    def get_nombre(self):
+        return self.nomb_cliente
+    def get_apellido(self):
+        return self.apell_cliente
+    def set_nombre(self,dato_nombre):
+        self.nomb_cliente=dato_nombre
+    def set_apell(self,dato_apellido):
+        self.apell_cliente=dato_apellido
+    
+    #Se crean metodos normales de la clase
+    def tomar_datos(self):
+        self.nomb_cliente=input("Digite el nombre de cliente: ")
+        self.apell_cliente=input("Digite el nombre de cliente: ")
+    def procesar_datos(self):
+        pass
+    def mostrar_datos(self):
+        aux=self.nomb_cliente+ " " +self.apell_cliente
+    def mostrar_datos(self):
+        print(f"Nombre cliente: {self.nomb_cliente} - Apellido cliente: {self.apell_cliente}")
+    def hacer_saludo(self,dato_saludo):
+        print(f"{dato_saludo} : {self.nomb_cliente} {self.apell_cliente}")
+
+#el contructor no retorna
---
+++ Principal.py
@@ -0,0 +1,9 @@
+from Cliente import Cliente
+from Saludo import Saludo
+
+objCliente=Cliente()
+objSaludo=Saludo()
+
+objCliente.tomar_datos()
+aux_mensaje=objSaludo.hacer_saludo_formal
+objCliente.hacer_saludo(aux_mensaje)
